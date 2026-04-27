a8037acdb3d6811485d977beabbcd2a5e56cc2b0
Ivanov Ilya <iiilya12@yandex.ru>
2026-04-28 01:04:37 +0300

    feat: add booking entity

---
+++ src/main/java/pet/airbooking/core/entity/BookingEntity.java
@@ -0,0 +1,75 @@
+package pet.airbooking.core.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import pet.airbooking.core.exception.IllegalBookingStatusException;
+import pet.airbooking.core.model.BookingStatus;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "booking")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookingEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Long userId;
+    private Long listingId;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    @Version
+    private long version;
+
+    public BookingEntity(Long userId, Long listingId) {
+        this.userId = userId;
+        this.listingId = listingId;
+        this.status = BookingStatus.PENDING;
+        this.createdAt = LocalDateTime.now();
+    }
+
+    public BookingEntity(Long userId, Long listingId, BookingStatus status) {
+        this.userId = userId;
+        this.listingId = listingId;
+        this.status = status;
+    }
+
+    public void confirm() {
+        if (getStatus() != BookingStatus.PENDING) {
+            throw new IllegalBookingStatusException("Cannot confirm booking");
+        } else {
+            setStatus(BookingStatus.CONFIRMED);
+        }
+    }
+
+    public void cancel() {
+        if (getStatus() != BookingStatus.PENDING) {
+            throw new IllegalBookingStatusException("Cannot cancel booking");
+        } else {
+            setStatus(BookingStatus.CANCELLED);
+        }
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+
+}
