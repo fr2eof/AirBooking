71e6ae3f8994d7b14a69eb916234a31929d63fbc
Ivanov Ilya <iiilya12@yandex.ru>
2026-04-29 16:18:03 +0300

    feat: add validation - on request params - add exceptions handlers

--- src/main/java/pet/airbooking/core/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/pet/airbooking/core/exception/handler/GlobalExceptionHandler.java
@@ -1,10 +1,13 @@
 package pet.airbooking.core.exception.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 import pet.airbooking.core.exception.EntityNotFoundException;
 import pet.airbooking.core.exception.IllegalBookingStatusException;
 import pet.airbooking.io.dto.response.ErrorResponse;
@@ -30,11 +33,49 @@ public class GlobalExceptionHandler {
         return buildResponse(HttpStatus.BAD_REQUEST, ex, request);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(
+            MethodArgumentNotValidException ex,
+            HttpServletRequest request
+    ) {
+        String message = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(err -> err.getField() + " " + err.getDefaultMessage())
+                .findFirst()
+                .orElse("Validation error");
+
+        return buildResponse(HttpStatus.BAD_REQUEST,
+                new RuntimeException(message),
+                request);
+    }
+
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<ErrorResponse> handleValidation(
+            HandlerMethodValidationException ex,
+            HttpServletRequest request
+    ) {
+
+        String message = ex.getParameterValidationResults()
+                .stream()
+                .flatMap(r -> r.getResolvableErrors().stream())
+                .map(error -> error.getDefaultMessage())
+                .findFirst()
+                .orElse("Validation error");
+
+        return buildResponse(
+                HttpStatus.BAD_REQUEST,
+                new RuntimeException(message),
+                request
+        );
+    }
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneric(
             Exception ex,
             HttpServletRequest request
     ) {
+        ex.printStackTrace();
         return buildResponse(HttpStatus.INTERNAL_SERVER_ERROR, ex, request);
     }
 
