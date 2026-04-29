0d14eb931cb9a0b9a2badbd4d2f1072ad2fd30d9
Ivanov Ilya <iiilya12@yandex.ru>
2026-04-29 14:44:36 +0300

    feat: add basic logging

--- src/main/java/pet/airbooking/io/messaging/impl/KafkaOutboxEventPublisher.java
+++ src/main/java/pet/airbooking/io/messaging/impl/KafkaOutboxEventPublisher.java
@@ -1,12 +1,14 @@
 package pet.airbooking.io.messaging.impl;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import pet.airbooking.core.entity.OutboxEvent;
 import pet.airbooking.core.model.EventType;
 import pet.airbooking.core.service.KafkaProducerService;
 import pet.airbooking.io.messaging.OutboxEventPublisher;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class KafkaOutboxEventPublisher implements OutboxEventPublisher {
@@ -16,11 +18,34 @@ public class KafkaOutboxEventPublisher implements OutboxEventPublisher {
     @Override
     public void publish(OutboxEvent event) {
 
-        kafkaProducerService.send(
-                resolveTopic(event.getEventType()),
-                String.valueOf(event.getAggregateId()),
-                event.getPayload()
-        );
+        String topic = resolveTopic(event.getEventType());
+
+        log.info("Publishing outbox event type={} aggregateId={} topic={}",
+                event.getEventType(),
+                event.getAggregateId(),
+                topic);
+
+        try {
+            kafkaProducerService.send(
+                    topic,
+                    String.valueOf(event.getAggregateId()),
+                    event.getPayload()
+            );
+
+            log.info("Outbox event published successfully type={} aggregateId={}",
+                    event.getEventType(),
+                    event.getAggregateId());
+
+        } catch (Exception ex) {
+
+            log.error("Failed to publish outbox event type={} aggregateId={} topic={}",
+                    event.getEventType(),
+                    event.getAggregateId(),
+                    topic,
+                    ex);
+
+            throw ex;
+        }
     }
 
     private String resolveTopic(EventType type) {
